a3569da2b021195a76e1f2907207c987b9d8581a
AscEmon <sayedemon53@gmail.com>
2021-01-16 22:39:36 +0600

    intent added for showing Popular Coffee

--- app/src/main/java/com/example/spinnervolition/MainActivity.java
+++ app/src/main/java/com/example/spinnervolition/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.spinnervolition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,9 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     String[] CountryNames;
     String[] CoffeeNames;
-    Spinner countrySpinner,coffeeSpinner;
+    Spinner countrySpinner;
     Button submitBtn;
-    TextView CoffeeAndCountry;
+    TextView popular;
+    String coffee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +25,58 @@ public class MainActivity extends AppCompatActivity {
         CountryNames=getResources().getStringArray(R.array.Country);
         CoffeeNames=getResources().getStringArray(R.array.Coffee);
 
-        coffeeSpinner=(Spinner) findViewById(R.id.CoffeeSpinner);
         countrySpinner=(Spinner) findViewById(R.id.CountrySpinner);
         submitBtn=(Button) findViewById(R.id.SubmitBtn);
-        CoffeeAndCountry=(TextView) findViewById(R.id.CountryAndCoffeeText);
+        popular=findViewById(R.id.popular);
+
 
         ArrayAdapter<String> countryAdapter=new ArrayAdapter<String>(this,R.layout.sample_view_country,R.id.textViewSampleId,CountryNames);
         countrySpinner.setAdapter(countryAdapter);
-        ArrayAdapter<String> coffeeAdapter=new ArrayAdapter<String>(this,R.layout.sample_view_coffee,R.id.textViewSampleId2,CoffeeNames);
-        coffeeSpinner.setAdapter(coffeeAdapter);
+
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+             //  String coffee=coffeeSpinner.getSelectedItem().toString();
                 String country=countrySpinner.getSelectedItem().toString();
-                String coffee=coffeeSpinner.getSelectedItem().toString();
-                CoffeeAndCountry.setText("Country is : "+country+"\n\nCoffee is : "+coffee);
+                if(country.equals("America"))
+                {
+                    coffee="Nescafe Extrafort";
+
+                }
+                else if(country.equals("Bangladesh"))
+                {
+                    coffee="Nescafe";
+
+                }
+                else if(country.equals("Canada"))
+                {
+                    coffee="David of";
+
+                }  else if(country.equals("Denmark"))
+                {
+                    coffee="Espresso";
+
+                }  else if(country.equals("England"))
+                {
+                    coffee="Cold brew";
+
+                }  else if(country.equals("France"))
+                {
+                    coffee="Latte";
+
+                }
+                 else if(country.equals("Ghana"))
+                {
+                    coffee="Hot Chocolate";
+
+                }
+
+               Intent intent=new Intent(getApplicationContext(),SecondPage.class);
+               intent.putExtra("value1",country);
+               intent.putExtra("value2",coffee);
+               startActivity(intent);
             }
         });
 
